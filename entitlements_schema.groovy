65fd8b9dc1eb62595b0a544912377f13a8c2019a
Tristan Partin <tristan.partin@experoinc.com>
2019-10-09 11:50:37 -0500

    Correct schema errors

--- entitlements_schema.groovy
+++ entitlements_schema.groovy
@@ -2,7 +2,7 @@ system.graph("entitlements").ifNotExists().create()
 
 :remote config alias g entitlements.g
 
-schema.vertexLabel("party").ifNotExists().partitionBy("party_id", Int).property("full_name", Text).property("email", Text).property("created_at", Date).property("updated_at").create()
+schema.vertexLabel("party").ifNotExists().partitionBy("party_id", Int).property("full_name", Text).property("email", Text).property("created_at", Date).property("updated_at", Date).create()
 schema.vertexLabel("identity").ifNotExists().partitionBy("identity_id", Int).property("name", Text).property("created_at", Date).create()
 
-schema.vertexLabel("is_a").tableName("is_a").ifNotExists().from("party").to("identity").partitionBy(OUT, "party_id", "party_id").clusterBy(IN, "identity_id", "identity_id").property("created_at", Date).create()
+schema.edgeLabel("is_a").tableName("is_a").ifNotExists().from("party").to("identity").partitionBy(OUT, "party_id", "party_id").clusterBy(IN, "identity_id", "identity_id").property("created_at", Date).create()
