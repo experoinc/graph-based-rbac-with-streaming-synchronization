6bc321d7e4c35557c64dfb302ab65b365a01509d
Tristan Partin <tristan@partin.io>
2019-11-04 13:52:00 -0600

    Updated configuration

--- entitlements_schema.groovy
+++
@@ -1,9 +0,0 @@
-system.graph("entitlements").ifNotExists().create()
-
-:remote config alias g entitlements.g
-
-schema.vertexLabel("party").ifNotExists().partitionBy("party_id", Int).property("full_name", Text).property("email", Text).property("created_at", Date).create()
-schema.vertexLabel("identity").ifNotExists().partitionBy("identity_id", Int).property("title", Text).property("created_at", Date).create()
-
-schema.edgeLabel("partyIdentity").tableName("partyIdentity").ifNotExists().from("party").to("identity").partitionBy(OUT, "party_id", "party_id").clusterBy(IN, "identity_id", "identity_id").property("created_at", Date).create()
-schema.edgeLabel("identityParty").tableName("identityParty").ifNotExists().from("identity").to("party").partitionBy(OUT, "identity_id", "identity_id").clusterBy(IN, "party_id", "party_id").property("created_at", Date).create()
