edd52cf56ddca65b683ce42f20959a06a85d3e43
Tristan Partin <tristan@partin.io>
2019-11-13 01:09:02 -0600

    Update for valid bit

--- LDAP.sql
+++ LDAP.sql
@@ -20,6 +20,7 @@ CREATE TABLE `LDAP`.`party_roles` (
 	`party_role_id` INT NOT NULL AUTO_INCREMENT,
 	`party_id` INT NOT NULL,
 	`role_id` INT NOT NULL,
+	`valid` BOOLEAN NOT NULL DEFAULT TRUE,
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`party_role_id`), UNIQUE `party_role` (`party_id`, `role_id`)) ENGINE = InnoDB;
@@ -28,6 +29,8 @@ ALTER TABLE `party_roles` ADD CONSTRAINT `party_id_fk` FOREIGN KEY (`party_id`)
 	REFERENCES `parties`(`party_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE `party_roles` ADD CONSTRAINT `role_id_fk` FOREIGN KEY (`role_id`)
 	REFERENCES `roles`(`role_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--- Run the inserts when you have the pipeline all set up
 
 INSERT INTO `roles` (`role_id`, `title`, `created_at`, `updated_at`) VALUES
 	(NULL, 'PRESIDENT', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()), (NULL, 'VICE_PRESIDENT', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
@@ -42,3 +45,5 @@ INSERT INTO `party_roles` (`party_id`, `role_id`, `created_at`, `updated_at`) VA
 	('1', '1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()), ('1', '4', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
 	('2', '3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()), ('2', '4', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
 	('3', '2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()), ('3', '4', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+--- Once the inserts have ran, try running these updates to see the changes
